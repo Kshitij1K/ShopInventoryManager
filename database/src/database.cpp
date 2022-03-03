13d0f3e766fe9f7effc3c32fb904093c650e11a2
Kshitij1K <kabeer@iitk.ac.in>
2022-03-03 21:49:21 +0530

    Add template for database function definitions

---
+++ database/src/database.cpp
@@ -0,0 +1,29 @@
+#include <database.hpp>
+
+void Database::updateEmployeeLogin(std::string employee_name, std::string time) {
+
+}
+
+void Database::updateEmployeeLogout(std::string employee_name, std::string time) {
+
+}
+
+AttendanceRecord Database::getEmployeeAttendance(std::string employee_name, std::string date) {
+
+}
+
+ItemUpdateResults Database::addNewItem(Item new_item) {
+
+}
+
+ItemUpdateResults Database::updateStock(Item item, long long int diff) {
+
+}
+
+void Database::updateItemsSold(Item item, std::string date, long long int quantity) {
+
+}
+
+long long int Database::getItemsSold(Item item, std::string date) {
+
+}
