082114efed0e4c3e4b78c0cb532194e0f40b5b03
Rishabh Kothary <rishabhkothary76@gmail.com>
2022-03-20 20:48:44 +0530

    code cleaned

--- state_machine/src/states/restocking_suggestion.cpp
+++ state_machine/src/states/restocking_suggestion.cpp
@@ -1,14 +1,14 @@
-//#include <all_shop_states.hpp>
+#include <all_shop_states.hpp>
 #include "ortools/linear_solver/linear_solver.h"
 #include <iostream>
-/*
+
 ShopState& RestockingSuggestion::getInstance() {
   static RestockingSuggestion singleton;
   return singleton;
 }
-*/
-namespace operations_research {
-std::vector<double> MipVarArray(long long n, double* buying_price,double* selling_price,double* holding_cost,long long* forecast, double capital) {
+
+namespace optimizer{
+std::vector<double> MipVarArray(long long n, std::vector<long double> buying_price,std::vector<long double> selling_price,std::vector<long double> holding_cost,std::vector<long long> forecast,long double capital) {
 
   std::vector<double> solution;
   // Create the mip solver with the SCIP backend.
@@ -65,25 +65,26 @@ std::vector<double> MipVarArray(long long n, double* buying_price,double* sellin
   return solution;
 }
 
-}  // namespace operations_research
-/*
+}  // namespace optimizer
+
 void RestockingSuggestion::enter(Shop* shop) {}
 void RestockingSuggestion::exit(Shop* shop) {}
-*/
+
  
 // For Debugging
-
+/*
 int main(int argc, char** argv) {
   std::vector<double>solution;
   long long n = 5;
-  double capital = 100000;
-  double selling_price[5] = {100,200,50,20,10};
-  double buying_price[5] = {80,160,40,12,7};
-  double holding_cost[5] ={20,30,40,50,60};
-  long long forecast[5]={10,15,20,25,40};
-  solution = operations_research::MipVarArray(n,buying_price,selling_price,holding_cost,forecast,capital);
+  long double capital = 100000;
+  std::vector<long double> selling_price = {100,200,50,20,10};
+  std::vector<long double> buying_price = {80,160,40,12,7};
+  std::vector<long double> holding_cost = {20,30,40,50,60};
+  std::vector<long long> forecast = {10,15,20,25,40};
+  solution = optimizer::MipVarArray(n,buying_price,selling_price,holding_cost,forecast,capital);
   for (int i =0 ;i<n;i++){
     std::cout<<solution[i]<<std::endl;
   }
   return EXIT_SUCCESS;
 }
+*/
