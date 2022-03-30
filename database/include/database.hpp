731370c630ed9a93e28937d5af940bd22eed81cc
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2022-03-30 21:14:37 +0530

    Implementing getAllItemStocks  adding__retrieve_predict_data_table  adding__database_Variable_path Database Changes:: Table Predict_table created and two rows added in Item_Info

--- database/include/database.hpp
+++ database/include/database.hpp
@@ -3,10 +3,7 @@
 #include <string>
 #include <utility>
 
-// The list consists of pairs of login and logout times. The first string is for
-// the login time, and the second one is for the logout time
-typedef std::list<std::pair<std::string, std::string>> AttendanceRecord;
-typedef std::list<std::pair<Item, long long int>> ItemStocks;
+
 
 enum class ItemUpdateResults {
   kExists,
@@ -23,8 +20,34 @@ struct Item {
   float holding_price;
 };
 
-class Database {
+struct Predict_data_type
+{
+    long long int item_id;
+    double Smoothed_error;
+    double Forecast;
+    double MADt;
+    double T;
+};
+
+// The list consists of pairs of login and logout times. The first string is for
+// the login time, and the second one is for the logout time
+typedef std::list<std::pair<std::string, std::string>> AttendanceRecord;
+typedef std::list<std::pair<Item, long long int>> ItemStocks;
+typedef std::list<Predict_data_type> Predict_record;
+
+
+
+class Database 
+{
+
  public:
+
+  string databse_var = "Dbmain.db";
+    //added later
+  sqlite3* establish_connection();
+
+
+
   void updateEmployeeLogin(std::string employee_name, std::string time,std::string date="");
   // void updateEmployeeLogin(std::string employee_name, std::string time);
 
@@ -43,12 +66,43 @@ class Database {
   long long int getItemsSold(Item item, std::string date);
 
   ItemStocks getAllItemStocks();  
+
+
+
+  void Insert_predict_data();
+  Predict_record retrieve_predict_data_basis_of_name(string name); //search on basis of item name
+  Predict_record retrieve_predict_data_basis_of_id(long long int); //search on basis of item_id
+
+
 };
 
 /*
 * Stuff to implement in database
-* Implement getAllItemStocks()
-* Implement forecasting data storage and retrieval
-* Add the buying price and holding price column to database
-* Change hardcoded location of database
+* Done :: Implement getAllItemStocks() 
+* Done(Insertion Pending) :: Implement forecasting data storage and retrieval
+* Done Add the buying price and holding price column to database
+* Done Change hardcoded location of database
 */
+
+
+
+
+/*
+* 
+* 
+*
+*
+* Pending
+* 
+* various optimistation 
+* connection_established function usage and removal of repeated code
+* Fucntion callback renaming
+* Variables for table name
+* void Insert_predict_data();
+*/
+
+
+
+
+
+
--- database/src/database.cpp
+++ database/src/database.cpp
@@ -1,4 +1,4 @@
-#include <database.hpp>
+// #include <database.hpp>
 #include <iostream>
 #include <sqlite3.h>
 #include <vector>
@@ -9,11 +9,38 @@ using namespace std;
 
 
 
+//for getting attendance data
 struct Data_v
 {
 	int sisze ;
 	vector<map<string, string>>data ;
 };
+
+//for getting Item_Info
+struct Data_for_Item
+{
+	int sisze ;
+    // list<std::pair<Item, long long int>> ItemStocks;
+    ItemStocks data1;
+
+};
+
+//for getting Item_Info
+struct Data_for_Predict
+{
+	int sisze ;
+    // typedef std::list<Predict_data_type> Predict_record;
+    Predict_record data1;
+
+};
+
+//getting Item_id
+struct Data_Item_List
+{
+	int sisze ;
+    vector<string>data ;
+};
+
 
 
 
@@ -56,15 +83,447 @@ static int callback1(void *data, int argc, char** argv, char** azColName)
 }
  
 
+static int callback2_getting_Item_data(void *data, int argc, char** argv, char** azColName)
+{
+    int i;
+	struct Data_for_Item *get_ans = (struct Data_for_Item*)data ;
+	get_ans->sisze++;
+
+    list<std::pair<Item, long long int>> var1;
+
+    vector<pair<string,string>> temp1;
+    Item t1 ;
+
+// struct Item {
+//   long long int item_id;
+//   std::string item_name;
+//   float selling_price;
+//   float buying_price;
+//   float holding_price;
+// };
+
+    long long int stock ;
+	string temp2,temp3 ;
+    for (i = 0; i < argc; i++) 
+    {
+        // printf("%s = %s ", azColName[i], argv[i] ? argv[i] : "NULL");
+
+		temp2 = azColName[i];
+		temp3 = argv[i] ? argv[i] : "NULL";
+        if(temp2.compare("ID")==0)
+        {
+            t1.item_id = stoll(temp3);
+        }
+        else if(temp2.compare("name")==0)
+        {
+            t1.item_name = temp3;
+        }
+        
+        else if(temp2.compare("price")==0)
+        {
+            if(temp3.compare("NULL")!=0)
+            t1.selling_price = stof(temp3);
+            else 
+            t1.selling_price = 0;
+        }
+        else if(temp2.compare("stock")==0)
+        {
+            stock = stoll(temp3);
+        }
+        else if(temp2.compare("Holding_value")==0)
+        {
+            
+            if(temp3.compare("NULL")!=0)
+            t1.holding_price = stof(temp3);
+            else 
+            t1.holding_price = 0;
+        }
+        else if(temp2.compare("Buying_value")==0)
+        {
+                        
+            if(temp3.compare("NULL")!=0)
+            t1.buying_price = stof(temp3);
+            else 
+            t1.buying_price = 0;
+        }
+            
+        
+        
+		// temp1[temp2] = temp3;
+    }
+    get_ans->data1.push_back({t1,stock});
+	// get_ans->data.push_back(temp1);
+    // printf("\n");
+	
+    return 0;
+}
+
+
+
+static int callback3_getting_Predict_data(void *data, int argc, char** argv, char** azColName)
+{
+    int i;
+	struct Data_for_Predict *get_ans = (struct Data_for_Predict*)data ;
+	get_ans->sisze++;
+
+
+
+// struct Predict_data_type
+// {
+//     long long int item_id;
+//     double Smoothed_error;
+//     double Forecast;
+//     double MADt;
+//     double T;
+// };
+// typedef std::list<Predict_data_type> Predict_record;
+
+
+// struct Data_for_Predict
+// {
+// 	int sisze ;
+//     // list<std::pair<Item, long long int>> ItemStocks;
+//     Predict_record data1;
+
+// };
+
+    Predict_data_type t1 ;
+
+	string temp2,temp3 ;
+    for (i = 0; i < argc; i++) 
+    {
+        printf("%s = %s ", azColName[i], argv[i] ? argv[i] : "NULL");
+
+		temp2 = azColName[i];
+		temp3 = argv[i] ? argv[i] : "NULL";
+        if(temp2.compare("Item_Id")==0)
+        {
+            t1.item_id = stoll(temp3);
+        }
+        else if(temp2.compare("Smoothed_Error")==0)
+        {
+            if(temp3.compare("NULL")!=0)
+            t1.Smoothed_error = stof(temp3);
+            else 
+            t1.Smoothed_error = 0;
+        }
+        
+        else if(temp2.compare("Forecast")==0)
+        {
+            if(temp3.compare("NULL")!=0)
+            t1.Forecast = stof(temp3);
+            else 
+            t1.Forecast = 0;
+        }
+
+        else if(temp2.compare("MADt")==0)
+        {
+            
+            if(temp3.compare("NULL")!=0)
+            t1.MADt = stof(temp3);
+            else 
+            t1.MADt = 0;
+        }
+        else if(temp2.compare("T")==0)
+        {
+                        
+            if(temp3.compare("NULL")!=0)
+            t1.T = stof(temp3);
+            else 
+            t1.T = 0;
+        }
+     
+		// temp1[temp2] = temp3;
+    }
+    get_ans->data1.push_back(t1);
+    printf("\n");
+    return 0;
+}
+
+
+
+static int callback4_getting_Item_ID(void *data, int argc, char** argv, char** azColName)
+{
+    int i;
+    // Data_Item_List
+	struct Data_Item_List *get_ans = (struct Data_Item_List*)data ;
+	get_ans->sisze++;
+;
+
+	string temp2,temp3 ;
+    for (i = 0; i < argc; i++) 
+    {
+        printf("%s = %s ", azColName[i], argv[i] ? argv[i] : "NULL");
+
+		temp2 = azColName[i];
+		temp3 = argv[i] ? argv[i] : "NULL";
+       
+		// temp1[temp2] = temp3;
+    }
+    
+    get_ans->data.push_back(temp3);
+    printf("\n");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+sqlite3* Database::establish_connection()
+{
+    sqlite3* DB;
+	int exit = 0;
+	// exit = sqlite3_open("Db1test", &DB);
+    exit = sqlite3_open(this->databse_var.c_str(),&DB);
+
+	if (exit) {
+		std::cout << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
+		// return (-1);
+	}
+	else
+		std::cout << "Opened Database Successfully!" << std::endl;
+
+    return DB;
+}
+
+
+
+
+
+
+
+
+
+//   Predict_record retrieve_predict_data_basis_of_id(long long int); //search on basis of item_id
+
+Predict_record Database:: retrieve_predict_data_basis_of_name(string name) //search on basis of item name
+{
+         //connection establised
+    sqlite3* DB;
+	int exit = 0;
+	// exit = sqlite3_open("Db1test", &DB);
+    exit = sqlite3_open(this->databse_var.c_str(),&DB);
+	if (exit) {
+		std::cout << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
+	}
+	else
+		std::cout << "Opened Database Successfully!" << std::endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// struct Data_Item_List
+// {
+// 	int sisze ;
+//     vector<string>data ;
+// };
+
+//     string id_val = to_string(id);
+    string query = "";
+    query = "SELECT ID FROM Item_Info WHERE name = '"+name + "';";
+    cout<<endl<<"SQL Query :"<<query<<endl;
+    struct Data_Item_List *get_ans = new Data_Item_List() ;
+    get_ans->sisze = 0;
+    char* messaggeError =NULL;
+    exit = sqlite3_exec(DB, query.c_str(), callback4_getting_Item_ID, get_ans, &messaggeError);
+
+
+    std::string string_message;
+    if (messaggeError==NULL)
+    {
+        string_message=="";
+        // cout<<"DEBUG1;";
+    }
+    else
+    {
+        string_message = messaggeError;
+    }
+        
+    cout<<endl ;   
+    vector<string>id_list = get_ans->data ;
+
+    string tuple_ids = "(";
+    for (auto in_id : id_list)
+    {
+        tuple_ids+=in_id;
+        tuple_ids+=",";
+    }
+    
+    tuple_ids = tuple_ids.substr(0,tuple_ids.size()-1);
+    tuple_ids+=")";
+    // cout<<tuple_ids<<endl;
+
+    free(get_ans);
+
+
+
+    query = "SELECT * FROM Predict_Table WHERE Item_Id IN "+tuple_ids + ";";
+
+	struct Data_for_Predict *get_ans1 = new Data_for_Predict() ;
+    get_ans1->sisze = 0;
+
+
+
+// // struct Predict_data_type
+// // {
+// //     long long int item_id;
+// //     double Smoothed_error;
+// //     double Forecast;
+// //     double MADt;
+// //     double T;
+// // };
+// // typedef std::list<Predict_data_type> Predict_record;
+
+
+// // struct Data_for_Predict
+// // {
+// // 	int sisze ;
+// //     // list<std::pair<Item, long long int>> ItemStocks;
+// //     Predict_record data1;
+
+// // };
+    cout<<endl<<"SQL Query :"<<query<<endl;
+    exit = sqlite3_exec(DB, query.c_str(), callback3_getting_Predict_data, get_ans1, &messaggeError);
+    if (messaggeError==NULL)
+    {
+        string_message=="";
+//         // cout<<"DEBUG1;";
+    }
+    else
+    {
+        string_message = messaggeError;
+    }
+        
+    cout<<endl ;    
+    Predict_record return_val1 = get_ans1->data1 ;
+    cout<<"\nOutput Message "<<string_message<<endl;
+    free(get_ans1);
+    sqlite3_close(DB);
+    return return_val1;
+}
+
+
+
+Predict_record Database:: retrieve_predict_data_basis_of_id(long long int id) //search on basis of item id
+{
+        //connection establised
+    sqlite3* DB;
+	int exit = 0;
+	// exit = sqlite3_open("Db1test", &DB);
+    exit = sqlite3_open(this->databse_var.c_str(),&DB);
+	if (exit) {
+		std::cout << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
+	}
+	else
+		std::cout << "Opened Database Successfully!" << std::endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    string id_val = to_string(id);
+    string query = "";
+    query = "SELECT * FROM Predict_Table WHERE Item_Id = "+id_val + ";";
+
+	struct Data_for_Predict *get_ans = new Data_for_Predict() ;
+    get_ans->sisze = 0;
+    char* messaggeError =NULL;
+
+
+
+// struct Predict_data_type
+// {
+//     long long int item_id;
+//     double Smoothed_error;
+//     double Forecast;
+//     double MADt;
+//     double T;
+// };
+// typedef std::list<Predict_data_type> Predict_record;
+
+
+// struct Data_for_Predict
+// {
+// 	int sisze ;
+//     // list<std::pair<Item, long long int>> ItemStocks;
+//     Predict_record data1;
+
+// };
+    cout<<endl<<"SQL Query :"<<query<<endl;
+    exit = sqlite3_exec(DB, query.c_str(), callback3_getting_Predict_data, get_ans, &messaggeError);
+    std::string string_message;
+    if (messaggeError==NULL)
+    {
+        string_message=="";
+        // cout<<"DEBUG1;";
+    }
+    else
+    {
+        string_message = messaggeError;
+    }
+        
+    cout<<endl ;    
+    Predict_record return_val1 = get_ans->data1 ;
+    cout<<"\nOutput Message "<<string_message<<endl;
+    free(get_ans);
+    sqlite3_close(DB);
+    return return_val1;
+}
+
+
+
 
 
 // void Database::updateEmployeeLogin(std::string employee_name, std::string time) 
 void Database::updateEmployeeLogin(std::string employee_name, std::string time,std::string date)
 {
-    string name = employee_name;
+
+    //connection establised
     sqlite3* DB;
 	int exit = 0;
-	exit = sqlite3_open("Db1test", &DB);
+	// exit = sqlite3_open("Db1test", &DB);
+    exit = sqlite3_open(this->databse_var.c_str(),&DB);
 	if (exit) {
 		std::cout << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
 		return ;
@@ -73,6 +532,12 @@ void Database::updateEmployeeLogin(std::string employee_name, std::string time,s
 		std::cout << "Opened Database Successfully!" << std::endl;
 
 
+
+
+
+
+
+    string name = employee_name;
     string query = "";
 
     string insert_1 = "INSERT INTO Emp_login_Info VALUES(";
@@ -112,15 +577,24 @@ void Database::updateEmployeeLogin(std::string employee_name, std::string time,s
 // void Database::updateEmployeeLogout(std::string employee_name, std::string time) 
 void Database::updateEmployeeLogout(string name, std::string time,string date) 
 {
+    //connection establised
     sqlite3* DB;
 	int exit = 0;
-	exit = sqlite3_open("Db1test", &DB);
+	// exit = sqlite3_open("Db1test", &DB);
+    exit = sqlite3_open(this->databse_var.c_str(),&DB);
 	if (exit) {
 		std::cout << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
 		return ;
 	}
 	else
 		std::cout << "Opened Database Successfully!" << std::endl;
+
+
+
+
+
+
+
 
     string query = "";
 
@@ -167,9 +641,9 @@ void Database::updateEmployeeLogout(string name, std::string time,string date)
 AttendanceRecord Database::getEmployeeAttendance(std::string employee_name, std::string date) 
 {
     sqlite3* DB;
-    std::list<std::pair<std::string, std::string>> ans1 ;
 	int exit = 0;
-	exit = sqlite3_open("Db1test", &DB);
+	// exit = sqlite3_open("Db1test", &DB);
+    exit = sqlite3_open(this->databse_var.c_str(),&DB);
 
 	if (exit) {
 		std::cout << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
@@ -178,6 +652,9 @@ AttendanceRecord Database::getEmployeeAttendance(std::string employee_name, std:
 	else
 		std::cout << "Opened Database Successfully!" << std::endl;
 
+
+
+    std::list<std::pair<std::string, std::string>> ans1 ;
     string query = "";
     query = "SELECT Login_T,Logout_T FROM Emp_Login_Info WHERE (Emp_Name = '"+employee_name+"' AND Date_val_login='"+date+"' );";
 	struct Data_v *get_ans = new Data_v() ;
@@ -216,16 +693,22 @@ AttendanceRecord Database::getEmployeeAttendance(std::string employee_name, std:
 
 ItemUpdateResults Database::addNewItem(Item new_item,long long int stock) 
 {
+    //connection establised
     sqlite3* DB;
 	int exit = 0;
-	exit = sqlite3_open("Db1test", &DB);
-
+	// exit = sqlite3_open("Db1test", &DB);
+    exit = sqlite3_open(this->databse_var.c_str(),&DB);
 	if (exit) {
 		std::cout << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
 		// return (-1);
 	}
 	else
 		std::cout << "Opened Database Successfully!" << std::endl;
+
+
+
+
+
 
     string query = "";
     string insert_1 = "INSERT INTO Item_Info VALUES(";
@@ -306,17 +789,25 @@ ItemUpdateResults Database::addNewItem(Item new_item,long long int stock)
 
 ItemUpdateResults Database::updateStock(Item new_item, long long int diff) 
 {
+    //connection establised
      sqlite3* DB;
 	int exit = 0;
     long long int sto_1 ;
-	exit = sqlite3_open("Db1test", &DB);
-
+	// exit = sqlite3_open("Db1test", &DB);
+    exit = sqlite3_open(this->databse_var.c_str(),&DB);
 	if (exit) {
 		std::cout << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
 		// return (-1);
 	}
 	else
 		std::cout << "Opened Database Successfully!" << std::endl;
+
+
+
+
+
+
+
 
     string current_id = to_string(new_item.item_id);
     string current_name = new_item.item_name;
@@ -446,10 +937,12 @@ ItemUpdateResults Database::updateStock(Item new_item, long long int diff)
 void Database::updateItemsSold(Item item, std::string date, long long int quantity) 
 {
 
+    //connection establised
     sqlite3* DB;
 	int exit = 0;
     long long int sto_1 ;
-	exit = sqlite3_open("Db1test", &DB);
+	// exit = sqlite3_open("Db1test", &DB);
+    exit = sqlite3_open(this->databse_var.c_str(),&DB);
 
 	if (exit) {
 		std::cout << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
@@ -457,6 +950,9 @@ void Database::updateItemsSold(Item item, std::string date, long long int quanti
 	}
 	else
 		std::cout << "Opened Database Successfully!" << std::endl;
+
+
+
 
 
     char* messaggeError =NULL;
@@ -599,11 +1095,12 @@ void Database::updateItemsSold(Item item, std::string date, long long int quanti
 
 long long int Database::getItemsSold(Item item, std::string date) 
 {
-       sqlite3* DB;
+    //connection establised
+    sqlite3* DB;
 	int exit = 0;
-    long long int Item_id = item.item_id;
     long long int sto_1 ;
-	exit = sqlite3_open("Db1test", &DB);
+	// exit = sqlite3_open("Db1test", &DB);
+    exit = sqlite3_open(this->databse_var.c_str(),&DB);
     string query ;
 	if (exit) {
 		std::cout << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
@@ -613,6 +1110,12 @@ long long int Database::getItemsSold(Item item, std::string date)
 		std::cout << "Opened Database Successfully!" << std::endl;
 
 
+
+
+
+
+
+    long long int Item_id = item.item_id;
     char* messaggeError =NULL;
     struct Data_v *get_ans = new Data_v() ;
     get_ans->sisze = 0;
@@ -655,3 +1158,155 @@ long long int Database::getItemsSold(Item item, std::string date)
     long long int val = stoll(quantity_1);
     return val ;
 }
+
+
+ItemStocks Database:: getAllItemStocks()
+{
+
+        //connection establised
+    sqlite3* DB;
+	int exit = 0;
+    long long int sto_1 ;
+	// exit = sqlite3_open("Db1test", &DB);
+    exit = sqlite3_open(this->databse_var.c_str(),&DB);
+    string query ;
+	if (exit) {
+		std::cout << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
+		// return (-1);
+	}
+	else
+		std::cout << "Opened Database Successfully!" << std::endl;
+
+
+
+//     struct Item {
+//   long long int item_id;
+//   std::string item_name;
+//   float selling_price;
+//   float buying_price;
+//   float holding_price;
+// };
+
+
+
+
+    // typedef std::list<std::pair<Item, long long int>> ItemStocks;
+
+    ItemStocks ans1;
+       char* messaggeError =NULL;
+    struct Data_for_Item *get_ans = new Data_for_Item() ;
+    get_ans->sisze = 0;
+    query = "SELECT * FROM Item_Info;";
+
+    cout<<endl<<"Sql Query :"<<query<<endl;
+    // cout<<get_ans->sisze<<endl;
+    
+    exit = sqlite3_exec(DB, query.c_str(), callback2_getting_Item_data, get_ans, &messaggeError);
+    if (get_ans->sisze==0)
+    {
+        cout<<"\n No such Data is present in Database";
+        sqlite3_close(DB);
+        // return -1;
+
+    }
+    // string quantity_1 = "";
+    // for (auto i1: get_ans->data )
+	// {	
+    //     quantity_1 = i1["Sold_quan"] ;
+	// 	cout<<endl<<quantity_1<<endl;
+	// }
+
+
+    std::string string_message;
+    if (messaggeError==NULL)
+    {
+        string_message=="";
+        // cout<<"DEBUG1;";
+    }
+    else
+    {
+        string_message = messaggeError;
+    }
+        
+    ans1 = get_ans->data1;
+    free(get_ans);
+    cout<<"\nOutput Message "<<string_message<<endl;
+    sqlite3_close(DB);
+    return ans1 ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//testing
+
+
+
+
+// int main(int argc, char const *argv[])
+// {
+//     Database d1 ;
+//     // AttendanceRecord ans = d1.getEmployeeAttendance("Sandeep Reddy","2022-03-09");
+
+//     // for (auto x : ans)
+//     // {
+//     //     cout<<"\n"<<x.first<<" "<<x.second ;
+//     // }
+
+
+
+
+
+
+//     // ItemStocks a1 = d1.getAllItemStocks();
+
+//     // for (auto x : a1)
+//     // {
+//     //     cout<<"\n"<<x.first.item_id<<" "<<x.first.item_name<<" "<<x.first.selling_price<<" "<<x.second<<" "<<x.first.holding_price <<" "<<x.first.buying_price ;   
+//     // }
+    
+//     // Predict_record a2 = d1.retrieve_predict_data_basis_of_id(4);
+//     // for (auto x :a2)
+//     // {
+//     //     cout<<x.item_id<<" "<<x.Smoothed_error<<" "<<x.Forecast<<" "<<x.MADt<<" "<<" "<<x.T<<endl;
+//     // }
+    
+//     Predict_record a2 = d1.retrieve_predict_data_basis_of_name("Maggi");
+//     for (auto x :a2)
+//     {
+//         cout<<x.item_id<<" "<<x.Smoothed_error<<" "<<x.Forecast<<" "<<x.MADt<<" "<<" "<<x.T<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+
