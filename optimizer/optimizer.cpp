c26f1eabb517430c00b1f66fb13bfe15dcfad701
Rishabh Kothary <rishabhkothary76@gmail.com>
2022-04-04 18:10:22 +0530

    update optimizer.cpp

--- optimizer/optimizer.cpp
+++ optimizer/optimizer.cpp
@@ -1,3 +1,4 @@
+
 #include "ortools/linear_solver/linear_solver.h"
 #include <vector>
 #include <fstream>
@@ -5,19 +6,21 @@
 #include <string>
 #include <sstream>
 
-std::vector<double> optimizer(std::vector<long long> current_inventory, std::vector<double> buying_price, std::vector<double> selling_price, std::vector<double> holding_cost, std::vector<double> forecast, long double capital) {
+
+
+using namespace std;
+void optimizer(std::vector<long long> current_inventory, std::vector<double> buying_price, std::vector<double> selling_price, std::vector<double> holding_cost, std::vector<double> forecast, long double capital) {
   // Create the mip solver with the SCIP backend.
   std::unique_ptr<MPSolver> solver(MPSolver::CreateSolver("SCIP"));
   if (!solver) {
     LOG(WARNING) << "SCIP solver unavailable.";
     return;
   }
-  std::vector<double> solution;
   long long n = current_inventory.size();
   const double infinity = solver->infinity();
   // x[j] is an array of non-negative, integer variables.
   std::vector<const MPVariable*> x(2*n);
-  for (int j = 0; j < n; ++j) {
+  for (long long int j = 0; j < n; ++j) {
     x[j] = solver->MakeIntVar(0.0, infinity, "");
     x[n+j] = solver->MakeIntVar(-infinity, infinity, "");
 
@@ -25,7 +28,7 @@ std::vector<double> optimizer(std::vector<long long> current_inventory, std::vec
   //LOG(INFO) << "Number of variables = " << solver->NumVariables();
 
   // Create the constraints.
-  for (int i = 0; i <n; ++i) {
+  for (long long int i = 0; i <n; ++i) {
     long double lower_bound_1,lower_bound_2;
     lower_bound_1 = holding_cost[i]*(current_inventory[i] - forecast[i]);
     lower_bound_2 = -selling_price[i]*(current_inventory[i] - forecast[i]);
@@ -38,14 +41,14 @@ std::vector<double> optimizer(std::vector<long long> current_inventory, std::vec
   }
 
   MPConstraint* constraint = solver->MakeRowConstraint(0,capital,"");
-  for (int i =0; i<n; ++i){
+  for (long long int i =0; i<n; ++i){
     constraint->SetCoefficient(x[i],buying_price[i]);
   }
   //LOG(INFO) << "Number of constraints = " << solver->NumConstraints();
 
   // Create the objective function.
   MPObjective* const objective = solver->MutableObjective();
-  for (int j = 0; j < n; ++j) {
+  for (long long int j = 0; j < n; ++j) {
     objective->SetCoefficient(x[j], -buying_price[j]);
     objective->SetCoefficient(x[n+j], -1);
   }
@@ -60,15 +63,19 @@ std::vector<double> optimizer(std::vector<long long> current_inventory, std::vec
   //LOG(INFO) << "Solution:";
   //LOG(INFO) << "Optimal objective value = " << objective->Value();
 
-  for (int j = 0; j <n; ++j) {
-    solution.push_back(x[j]->solution_value());
+
+  ofstream datafile;
+  datafile.open("data.txt",ofstream:: trunc);
+  for(long long int i=0;i<n;i++){
+    datafile << x[i]->solution_value() <<endl;
   }
-  return(solution);
+
+  return;
 
 }
 
 using namespace std;
-vector<double> read_and_optimize(){
+void read_and_optimize(){
   ifstream datafile;
   datafile.open("data.txt");
   string data;
@@ -123,15 +130,38 @@ vector<double> read_and_optimize(){
       i++;
     }
   }
-  return(optimizer(current_inventory,buying_price,selling_price,holding_cost,forecast,capital));
+  optimizer(current_inventory,buying_price,selling_price,holding_cost,forecast,capital);
+  return;
   
 }
 
-void write_the_solution(vector<double> solution){
-  ofstream datafile;
-  datafile.open("data.txt",ofstream:: trunc);
-  for(int i=0;i<solution.size();i++){
-    datafile << solution[i]<<endl;
-  }
-  return;
+
+int main(){
+  read_and_optimize();
+  return(0);
 }
+*/
+
+
+/*
+int main(int argc, char** argv) {
+  std::vector<double> current_inventory;
+  std::vector<double> buying_price,selling_price,holding_cost,forecast;
+  long double capital = 10000;
+  current_inventory  = {10,20,30};
+  buying_price = {120,130,140};
+  selling_price = {140,150,160};
+  holding_cost = {5,5,5};
+  forecast = {40,40,40};
+
+  MipVarArray(current_inventory, buying_price,selling_price,holding_cost,forecast,capital);
+  return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
+
